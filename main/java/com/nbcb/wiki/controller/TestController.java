8caee2e3591a4b1930b83863b731255eed92b59f
tisky <tisky09@gmail.com>
2022-08-23 21:34:48 +0800

    2-7 client-http

--- main/java/com/nbcb/wiki/controller/TestController.java
+++ main/java/com/nbcb/wiki/controller/TestController.java
@@ -2,14 +2,23 @@ package com.nbcb.wiki.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 
 public class TestController {
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+
+        return "hello World! Post,"+name;
     }
 }
