ba1de41444d00999419d8c4e3304af0967dd316a
tisky <tisky09@gmail.com>
2022-08-23 21:01:06 +0800

    2-7 hello接口

--- main/java/com/nbcb/wiki/WikiApplication.java
+++ main/java/com/nbcb/wiki/WikiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+
 public class WikiApplication {
 	private static  final Logger LOG=LoggerFactory.getLogger(WikiApplication.class);
 	public static void main(String[] args) {
