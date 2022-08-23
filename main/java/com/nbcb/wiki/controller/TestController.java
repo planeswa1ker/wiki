10a62ae21ec5201923fbd14b65cdaacb745536e4
tisky <tisky09@gmail.com>
2022-08-23 22:00:10 +0800

    2-8 自定义配置项

--- main/java/com/nbcb/wiki/controller/TestController.java
+++ main/java/com/nbcb/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.nbcb.wiki.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,13 @@ import java.util.Map;
 @RestController
 
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!"+testHello;
     }
 
     @PostMapping("/hello/post")
